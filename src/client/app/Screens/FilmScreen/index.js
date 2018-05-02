1892c23fdb50a839df36e68e8ca9f0bdfdc81499
Alena Hrytsuk <Alena_Hrytsuk@epam.com>
2018-05-02 20:17:15 +0300

    add mockArray for films

--- src/client/app/Screens/FilmScreen/index.js
+++ src/client/app/Screens/FilmScreen/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import Footer  from '../../components/Footer';
 import ProposedFilmsContainer from './components/ProposedFilmsContainer.jsx';
 import FilmInfoContainer from './components/FilmInfoContainer.jsx';
+import { filmsMockArray } from '../../mocks';
 
 class FilmScreen extends React.Component {
   constructor(props) {
@@ -15,23 +16,7 @@ class FilmScreen extends React.Component {
       year: '1994',
       duration: 54,
       description: 'Kill Bill: Volume 1 is a 2003 American martial arts film written and directed by Quentin Tarantino. It stars Uma Thurman as the Bride, who swears revenge on a team of assassins (Lucy Liu, Michael Madsen, Daryl Hannah, and Vivica A. Fox) and their leader Bill (David Carradine) after they try to kill her and her unborn child. Her journey takes her to Japan, where she battles the Tokyo yakuza.',
-      films: [
-        {
-          name: 'Kill Bill',
-          imgSrc: 'http://www.imfdb.org/images/thumb/6/65/Killbillposter.jpg/300px-Killbillposter.jpg',
-          genre: 'drama',
-          year: 2014,
-          id: 1
-        },
-        {
-          name: 'Kill Bill2',
-          imgSrc: 'http://www.imfdb.org/images/thumb/6/65/Killbillposter.jpg/300px-Killbillposter.jpg',
-          genre: 'adventure',
-          year: 2017,
-          id: 2
-        }
-      ]
-
+      films: filmsMockArray
     }
   }
 
--- src/client/app/Screens/SearchPage/components/SearchResultsContainer.jsx
+++ src/client/app/Screens/SearchPage/components/SearchResultsContainer.jsx
@@ -3,28 +3,14 @@ import { render } from 'react-dom';
 import Footer  from '../../../components/Footer';
 import SearchResultsHeader  from './SearchResultsHeader.jsx';
 import ResultsList  from '../../../components/ResultsList';
+import { filmsMockArray } from '../../../mocks';
 
 class SearchResultsContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       resultsNumber: 7,
-      films: [
-        {
-          name: 'Kill Bill',
-          imgSrc: 'http://www.imfdb.org/images/thumb/6/65/Killbillposter.jpg/300px-Killbillposter.jpg',
-          genre: 'drama',
-          year: 2014,
-          id: 1
-        },
-        {
-          name: 'Kill Bill2',
-          imgSrc: 'http://www.imfdb.org/images/thumb/6/65/Killbillposter.jpg/300px-Killbillposter.jpg',
-          genre: 'adventure',
-          year: 2017,
-          id: 2
-        }
-      ]
+      films: filmsMockArray
     }
   }
   render () {
